107f34cd82934534a6d585b81c1488cfde83a723
jkmiva <10102140241@stu.ecnu.edu.cn>
2016-12-11 16:58:11 -0600

    leetcode 101

---
+++ leetcode/symmetricTree.cc
@@ -0,0 +1,32 @@
+// Copyright [2016] <Huang Jiaming>
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+        return helper(root->left, root->right);
+    }
+
+    bool helper(TreeNode* a, TreeNode* b) {
+        if (!a && !b) {
+            return true;
+        }
+        else if (!a || !b) {
+            return false;
+        }
+        if (a->val == b->val) {
+            return helper(a->left, b->right) && helper(a->right, b->left);
+        }
+        else {
+            return false;
+        }
+    }
+};
