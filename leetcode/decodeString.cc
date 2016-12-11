c2f008b722131b08f556b2d27ef045531bfef369
jkmiva <10102140241@stu.ecnu.edu.cn>
2016-12-11 16:41:41 -0600

    leetcode 394

---
+++ leetcode/decodeString.cc
@@ -0,0 +1,37 @@
+// Copyright [2016] <Huang Jiaming>
+class Solution {
+    /*
+     * one-pass solution with an index reference.
+     *
+     */
+  public:
+    string decodeString(string s, int& i) {
+        string ret;
+
+        while (i < s.size() && s[i] != ']') {
+            if (!isdigit(s[i]))
+                ret += s[i++];
+            else {
+                int n = 0;
+                while (i < s.size() && isdigit(s[i]))
+                    n = n * 10 + s[i++] - '0';
+
+                i++;  // skip '['
+                string p = decodeString(s, i);  // get pure alphabet string
+                i++;  // skip ']'
+
+                while (n > 0) {
+                    ret += p;
+                    n--;
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    string decodeString(string s) {
+        int i = 0;
+        return decodeString(s, i);
+    }
+};
